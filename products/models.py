da5bc7d13becfd13d70c78fd09d51f538fb37243
Sultan <sultanbek9899@gmail.com>
2020-08-28 20:40:16 +0600

    added new models

--- products/models.py
+++ products/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, unique=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category=models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, db_index=True)
+    image = models.ImageField(upload_to='products/%Y/%m/%d')
+    description= models.TextField(null=True`)
+    price = models.DecimalField(max_digits=10,  decimal_places=2)
+    created=models.DateTimeField(auto_now_add=True)
+    status=models.BooleanField(default=True)
+
+    class Meta:
+        ordering=('name',)
+        index_together=(('id', 'slug'),)
+
+    def __str__(self):
+        return self.name
