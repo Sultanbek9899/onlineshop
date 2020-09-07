b33ba09bed019c3de10564fb73f63808f9fbc644
Sultan <sultanbek9899@gmail.com>
2020-09-07 17:58:38 +0600

    debug  False

--- store/settings.py
+++ store/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = local_settings.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
